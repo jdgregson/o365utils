06f14bcbfff724729df2f20b1955240784ee92b5
Jonathan Gregson <jdgregson@gmail.com>
2017-07-24 23:41:16 -0700

    Added script and readme

---
+++ Delete-Emails.ps1
@@ -0,0 +1,77 @@
+# File: Delete-Emails.ps1
+# Purpose: this script will connect to the Office 365 Security & Compliance
+# Center and will allow admins to delete specified emails from every mailbox
+# in the organization.
+# Author: Jonathan Gregson <jonathan.gregson@agsstainless.com>
+#                          <jdgregson@gmail.com>
+
+# connect to Office 365 Security & Compliance Center
+try{$out = Get-ComplianceSearch|Out-String} catch {
+    Write-Host "Enter your Office 365 admin user credentials..."
+    $UserCredential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid -Credential $UserCredential -Authentication Basic -AllowRedirection
+    Import-PSSession $Session -AllowClobber -DisableNameChecking
+    $Host.UI.RawUI.WindowTitle = $UserCredential.UserName + " (Office 365 Security & Compliance Center)"
+}
+
+# get the search criteria from the user
+Write-Host "Enter a seach string to locate the email(s)"
+Write-Host 'Example: sent>=07/03/2017 AND sent<=07/05/2017 AND subject:"open this attachment!!"'
+$search = Read-Host "Search"
+
+# create and run the search
+$guid = [guid]::NewGuid().Guid.Replace("-", "").Substring(25)
+$out = New-ComplianceSearch -Name "$guid" -ExchangeLocation all -ContentMatchQuery "$search" | Out-String
+Write-Host "Starting the search..."
+Start-ComplianceSearch -Identity "$guid"
+
+# wait for the results and ask the user if they look right
+$searchCompleted = $false
+$timeout = 60
+For ($i=0; $i -le $timeout; $i++) {
+    $theSearch = Get-ComplianceSearch -Identity "$guid" | Out-String
+    $searchProgress = $theSearch | Select-String -pattern "Completed"
+    if($searchProgress.length -gt 0) {
+        $searchCompleted = $true
+        Write-Host "Search complete"
+        Write-Host "The search returned the following:"
+        Get-ComplianceSearch -Identity "$guid" | Format-List -Property Items
+        Write-Host "Does this seem accurate?"
+        Write-Host "[Y] Yes [N] No [M] More details - default No"
+        $answer = Read-Host "Confirm"
+        if($answer.ToLower() -eq "y") {
+            Write-Host "Confirmed. Continuing to delete..."
+            break
+        } elseif($answer.ToLower() -eq "m") {
+            Get-ComplianceSearch -Identity "$guid" | Format-List
+            continue;
+        } else {
+            Write-Host "Canceled. Cleaning up and exiting..."
+            Remove-ComplianceSearch -Identity "$guid" -Confirm:$false
+            Exit
+        }
+    }
+    Sleep 1
+}
+if($searchCompleted -eq $false) {
+    Write-Host "Error: the search timed out"
+    Remove-ComplianceSearch -Identity "$guid" -Confirm:$false
+    Exit
+}
+
+# delete the emails with the user's confirmation
+$out = New-ComplianceSearchAction -SearchName "$guid" -Purge -PurgeType SoftDelete | Out-String
+
+# wait for the deletion results and delete the search if it is
+$timeout = 120
+For ($i=0; $i -le $timeout; $i++) {
+    $thePurge = Get-ComplianceSearchAction -Identity $guid"_Purge" | Out-String
+    $purgeProgress = $thePurge | Select-String -Pattern "Completed"
+    if($purgeProgress.length -gt 0) {
+        Write-Host "Deletion complete"
+        Write-Host "Cleaning up and exiting..."
+        Remove-ComplianceSearch -Identity "$guid" -Confirm:$false
+        Exit
+    }
+    Sleep 1
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Delete Emails
+# Delete Emails (O365)
+Delete-Emails is a PowerShell script used to automate the process of deleting
+emails from multiple Office 365 mailboxes at once. Useful for removing malicious
+or phishing emails, as well as a more effective method of recalling emails.
+
+## What it Does
+Delete-Emails uses the steps discussed [here](https://support.office.com/en-us/article/3526fd06-b45f-445b-aed4-5ebd37b3762a)
+to connect to the Office 365 Security & Compliance Center, perform a search 
+using your criteria, and delete the emails that it finds (after confirmation, of 
+course).
+
+`````/!\ WARNING`````
+`````Delete Emails is very powerful, likely more powerful than it needs to `````
+`````be. If you were to search for and delete emails that were sent to `````
+`````brandon@yourcompany.com with no further search limits (such as date `````
+`````and subject), this would delete every email that has ever been sent to`````
+````` Brandon by any user, inside or outside of your company, including `````
+`````emails in other users' inboxes and sent folders (including replys and`````
+````` CC's), junk folders, or deleted items folders. This script has the `````
+`````power to remove every single email that has ever been sent in your `````
+`````Office 365 tenant. `````
+
+## Requirements
+- Delete-Emails uses some featues in PowerShell v5. If you are using Windows 7
+or Windows 8.1, please make sure you have installed [this](https://www.microsoft.com/en-us/download/details.aspx?id=50395)
+  Windows update.
+- This script was developed using a full Office 365 admin account. If you have
+limited privelages in O365, you may have difficulty using this script.
